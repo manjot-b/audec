c487601c11218d40c6ae297a94223c8fcd52a84b
Manjot <mbal29@hotmail.com>
2021-11-12 18:32:54 -0700

    Decode 16-bit pcm audio.

--- audec/decoder.c
+++ audec/decoder.c
@@ -3,6 +3,8 @@
  * the output buffer.
  */
 
+#include <stdint.h>
+
 #include <FreeRTOS.h>
 #include <queue.h>
 
@@ -10,14 +12,35 @@
 #include "buffers.h"
 #include "taskdata.h"
 
+static void decode(const InfoPacket* info);
+static void decode16(const InfoPacket* info);
+
+static void decode(const InfoPacket* info) {
+	switch (info->bitDepth) {
+	case 16:
+		decode16(info);
+		break;
+	}
+}
+
+static void decode16(const InfoPacket* info) {
+	for (size_t inp = 0, out = 0; inp < info->dataLength; inp += 2, out++) {
+		int32_t spcm = g_inputBuf[inp] | (g_inputBuf[inp + 1] << 8);
+		spcm += 1 << 15;
+		uint16_t pcm = (uint16_t)spcm;
+
+		// Only a 12-bit DAC
+		pcm = pcm >> 4;
+		g_outputBuf[out] = pcm;
+	}
+}
+
 void decoderTask(void*) {
 	InfoPacket info;
 
 	for(;;) {
 		xTaskNotifyGiveIndexed(taskData.hostIOHandle, HOSTIO_NOTIFICATION_DECODER);
 		xQueueReceive(taskData.decoderQueue, &info, portMAX_DELAY);
-		while(1) {
-			taskYIELD();
-		}
+		decode(&info);
 	}
 }
